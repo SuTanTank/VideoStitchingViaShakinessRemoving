ea5e647b879fd32819e0cd152f8b8abfce1f7f7c
Tan <sutank922@gmail.com>
2018-02-05 23:07:55 +0800

    add script for extracting frames

---
+++ case-cuhk_lib/video2frames.m
@@ -0,0 +1,22 @@
+video = VideoReader('./case17-l.mp4');
+if ~exist('left', 'dir')
+    mkdir('left');
+end
+k = 0;
+while hasFrame(video)
+    k = k + 1;    
+    frame = readFrame(video);
+    filename = ['./left/' sprintf('%03d',k) '.png'];
+    imwrite(frame, filename);    
+end
+video = VideoReader('./case17-r.mp4');
+if ~exist('right', 'dir')
+    mkdir('right');
+end
+k = 0;
+while hasFrame(video)
+    k = k + 1;
+    frame = readFrame(video);
+    filename = ['./right/' sprintf('%03d',k) '.png'];
+    imwrite(frame, filename);    
+end
